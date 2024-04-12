63b28140f74588dc1e56f81400d4dc92cdd08e4c
Asus <abhijeet.jmt108@gmail.com>
2024-04-12 11:24:11 +0530

    pdf convertor added line

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ for index,row in df.iterrows():
     pdf.set_font(family="Times", style="B",size=12)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0,h=12,txt=row["Topic"],align="L",ln=1)
+    pdf.line(10,20, 200,20)
 
 
 pdf.output("output.pdf")
